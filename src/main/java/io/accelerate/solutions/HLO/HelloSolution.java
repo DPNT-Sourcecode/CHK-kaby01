5d4d82776391b59f813e24762afe40b6d100b425
sbx_user1051 <sbx_user1051@169.254.84.205>
2025-10-19 10:16:45 +0000

    Sun Oct 19 10:16:45 UTC 2025

--- src/main/java/io/accelerate/solutions/HLO/HelloSolution.java
+++ src/main/java/io/accelerate/solutions/HLO/HelloSolution.java
@@ -1,9 +1,8 @@
 package io.accelerate.solutions.HLO;
 
-import io.accelerate.runner.SolutionNotImplementedException;
-
 public class HelloSolution {
     public String hello(String friendName) {
         return String.format("Hello, %s!", friendName);
     }
 }
+
--- src/test/java/io/accelerate/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/io/accelerate/solutions/HLO/HelloSolutionTest.java
@@ -1,2 +1,25 @@
-package io.accelerate.solutions.HLO;public class HelloSolutionTest {
+package io.accelerate.solutions.HLO;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class HelloSolutionTest {
+    private HelloSolution helloSolution;
+
+    @BeforeEach
+    void setHelloSolution() {
+        helloSolution = new HelloSolution();
+    }
+
+    @Test
+    void testHelloSolution() {
+        var name = "Mr. X";
+        var result = helloSolution.hello(name);
+        assertThat(result, equalTo("Hello, Mr. X!"));
+
+    }
 }
+
