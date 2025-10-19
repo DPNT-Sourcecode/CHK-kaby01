e352eec3344f08a7fd8ce5a75f13fd69375e8422
sbx_user1051 <sbx_user1051@169.254.84.205>
2025-10-19 10:24:13 +0000

    Sun Oct 19 10:24:13 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -3,7 +3,18 @@ package io.accelerate.solutions.CHK;
 import io.accelerate.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
+//    Our price table and offers:
+//            +------+-------+----------------+
+//            | Item | Price | Special offers |
+//            +------+-------+----------------+
+//            | A    | 50    | 3A for 130     |
+//            | B    | 30    | 2B for 45      |
+//            | C    | 20    |                |
+//            | D    | 15    |                |
+//            +------+-------+----------------+
     public Integer checkout(String skus) {
+        if (skus == null || skus )
         throw new SolutionNotImplementedException();
     }
 }
+
