7bd7db8f7f51c7d22afc295aa47dc5bab355f14e
sbx_user1051 <sbx_user1051@169.254.84.205>
2025-10-19 10:51:13 +0000

    Sun Oct 19 10:51:13 UTC 2025

--- src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
@@ -3,6 +3,9 @@ package io.accelerate.solutions.CHK;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class CheckoutSolutionTest {
     private CheckoutSolution checkoutSolution;
 
@@ -14,6 +17,26 @@ public class CheckoutSolutionTest {
     @Test
     void invalidItem() {
         var item = "E";
-        checkoutSolution.checkout(E)
+        var result = checkoutSolution.checkout(item);
+        assertThat(result, equalTo(-1));
     }
+//    Our price table and offers:
+//            +------+-------+----------------+
+//            | Item | Price | Special offers |
+//            +------+-------+----------------+
+//            | A    | 50    | 3A for 130     |
+//            | B    | 30    | 2B for 45      |
+//            | C    | 20    |                |
+//            | D    | 15    |                |
+//            +------+-------+----------------+
+
+    @Test
+    void validItem() {
+        var items = "ABCD";
+        var result = checkoutSolution.checkout(items);
+        assertThat(result, equalTo(115));
+
+    }
+
 }
+
