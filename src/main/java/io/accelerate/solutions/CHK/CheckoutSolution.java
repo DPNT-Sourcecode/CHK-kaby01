8171e74da9ff7bdf25e6c3fc4d8e9bfd7ab909b0
sbx_user1051 <sbx_user1051@169.254.84.205>
2025-10-19 13:23:54 +0000

    Sun Oct 19 13:23:54 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -5,36 +5,38 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-//+------+-------+------------------------+
-//        | Item | Price | Special offers         |
-//        +------+-------+------------------------+
-//        | A    | 50    | 3A for 130, 5A for 200 |
-//        | B    | 30    | 2B for 45              |
-//        | C    | 20    |                        |
-//        | D    | 15    |                        |
-//        | E    | 40    | 2E get one B free      |
-//        | F    | 10    | 2F get one F free      |
-//        | G    | 20    |                        |
-//        | H    | 10    | 5H for 45, 10H for 80  |
-//        | I    | 35    |                        |
-//        | J    | 60    |                        |
-//        | K    | 80    | 2K for 150             |
-//        | L    | 90    |                        |
-//        | M    | 15    |                        |
-//        | N    | 40    | 3N get one M free      |
-//        | O    | 10    |                        |
-//        | P    | 50    | 5P for 200             |
-//        | Q    | 30    | 3Q for 80              |
-//        | R    | 50    | 3R get one Q free      |
-//        | S    | 30    |                        |
-//        | T    | 20    |                        |
-//        | U    | 40    | 3U get one U free      |
-//        | V    | 50    | 2V for 90, 3V for 130  |
-//        | W    | 20    |                        |
-//        | X    | 90    |                        |
-//        | Y    | 10    |                        |
-//        | Z    | 50    |                        |
-//        +------+-------+------------------------+
+
+//Our price table and offers:
+//        +------+-------+---------------------------------+
+//        | Item | Price | Special offers                  |
+//        +------+-------+---------------------------------+
+//        | A    | 50    | 3A for 130, 5A for 200          |
+//        | B    | 30    | 2B for 45                       |
+//        | C    | 20    |                                 |
+//        | D    | 15    |                                 |
+//        | E    | 40    | 2E get one B free               |
+//        | F    | 10    | 2F get one F free               |
+//        | G    | 20    |                                 |
+//        | H    | 10    | 5H for 45, 10H for 80           |
+//        | I    | 35    |                                 |
+//        | J    | 60    |                                 |
+//        | K    | 70    | 2K for 120                      |
+//        | L    | 90    |                                 |
+//        | M    | 15    |                                 |
+//        | N    | 40    | 3N get one M free               |
+//        | O    | 10    |                                 |
+//        | P    | 50    | 5P for 200                      |
+//        | Q    | 30    | 3Q for 80                       |
+//        | R    | 50    | 3R get one Q free               |
+//        | S    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+//        | T    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+//        | U    | 40    | 3U get one U free               |
+//        | V    | 50    | 2V for 90, 3V for 130           |
+//        | W    | 20    |                                 |
+//        | X    | 17    | buy any 3 of (S,T,X,Y,Z) for 45 |
+//        | Y    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+//        | Z    | 21    | buy any 3 of (S,T,X,Y,Z) for 45 |
+//        +------+-------+---------------------------------+
 
 
 public class CheckoutSolution {
@@ -44,12 +46,12 @@ public class CheckoutSolution {
             Map.entry('A', 50), Map.entry('B', 30), Map.entry('C', 20),
             Map.entry('D', 15), Map.entry('E', 40), Map.entry('F', 10),
             Map.entry('G', 20), Map.entry('H', 10), Map.entry('I', 35),
-            Map.entry('J', 60), Map.entry('K', 80), Map.entry('L', 90),
+            Map.entry('J', 60), Map.entry('K', 70), Map.entry('L', 90),
             Map.entry('M', 15), Map.entry('N', 40), Map.entry('O', 10),
             Map.entry('P', 50), Map.entry('Q', 30), Map.entry('R', 50),
-            Map.entry('S', 30), Map.entry('T', 20), Map.entry('U', 40),
-            Map.entry('V', 50), Map.entry('W', 20), Map.entry('X', 90),
-            Map.entry('Y', 10), Map.entry('Z', 50)
+            Map.entry('S', 20), Map.entry('T', 20), Map.entry('U', 40),
+            Map.entry('V', 50), Map.entry('W', 20), Map.entry('X', 70),
+            Map.entry('Y', 20), Map.entry('Z', 21)
     );
     private static final Map<Character, List<SpecialOffer>> SPECIAL_OFFERS = Map.of(
             'A', List.of(new SpecialOffer(3, 130), new SpecialOffer(5, 200)),
@@ -164,3 +166,4 @@ public class CheckoutSolution {
     }
 
 }
+
